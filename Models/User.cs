d532f42fcae630ed733b233bb3365c9ffaeb0694
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-02-19 22:35:32 +0800

    Added sample model User.

---
+++ Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ibsgateway831;
+
+namespace CoreNancySelfHostedNet.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string Username { get; private set; }
+
+        public User(string username = null)
+        {
+            Username = username;
+        }
+
+        public GatewayClass CreateGateway()
+        {
+            GatewayClass gw = new GatewayClass();
+            gw.setpath("D:\\Apps\\Infusion\\--DEMO--");
+
+            return gw;
+        }
+    }
+}
