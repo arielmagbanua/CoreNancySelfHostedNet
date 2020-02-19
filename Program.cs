9f4f8f7495007ae988fea1cb29d819cf39bee90a
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-02-19 22:39:04 +0800

    Added the Program module which serves as the entry point of the web service.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy.Hosting.Self;
+
+namespace CoreNancySelfHostedNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hostConfigs = new HostConfiguration
+            {
+                UrlReservations = new UrlReservations()
+                {
+                    CreateAutomatically = true
+                }
+            };
+
+            Uri uri = new Uri("http://localhost:1234");
+
+            using (var host = new NancyHost(hostConfigs, uri))
+            {
+                host.Start();
+                Console.WriteLine("Running on http://localhost:1234");
+                Console.ReadLine();
+            }
+        }
+    }
+}
