8cb7c9a5fe55b0abeabf82132df61dab3ec1493e
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-02-19 22:36:59 +0800

    Implemented sateless authentication.

---
+++ Auth/Authenticator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreNancySelfHostedNet.Auth
+{
+    public class Authenticator
+    {
+        public static ClaimsPrincipal AuthenticateToken(string token, string key)
+        {
+            string credentials = "super-secret-credentials-lol!";
+            
+
+            if (token == null || !token.Equals(credentials))
+            {
+                return null;
+            }
+
+            return new ClaimsPrincipal(new GenericIdentity(token, "stateless"));
+        }
+    }
+}
